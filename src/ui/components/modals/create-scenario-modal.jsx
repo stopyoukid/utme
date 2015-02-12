d3dc498acf3937be0558c0a53bcdf00a32a94427
Steven Lambion <steven.lambion@atsid.com>
2015-02-12 09:39:32 -0800

    Added UtmeMixin to create-scenario-modal.

--- src/ui/components/modals/create-scenario-modal.jsx
+++ src/ui/components/modals/create-scenario-modal.jsx
@@ -4,33 +4,36 @@ var Modal = bs.Modal;
 var Input = bs.Input;
 var Button = bs.Button;
 var modalLauncher = require('./modal-launcher.jsx');
+var UtmeMixin = require('../mixins/UtmeMixin');
 
 var CreateModal = React.createClass({
-    render: function () {
+    mixins: [UtmeMixin],
+
+    render () {
         return (
-            <Modal title="Save Scenario Recording" onRequestHide={function () {}} className="utme-create-modal" data-ignore="true">
-                <div className="modal-body" data-ignore="true">
+            <Modal title="Save Scenario Recording" onRequestHide={() => {}} className="utme-create-modal">
+                <div className="modal-body">
                     <form data-ignore="true" ref="form">
-                        <Input type="text" label="Scenario Name" ref="scenarioName" data-ignore="true"/>
-                        <Input type="text" label="Description (Optional):" ref="description" data-ignore="true"/>
-                        <Input type="text" label="Setup Scenarios (Optional, Newline separated):" ref="setupScenarios" data-ignore="true"/>
-                        <Input type="text" label="Cleanup Scenarios (Optional, Newline separated):" ref="cleanupScenarios" data-ignore="true"/>
+                        <Input type="text" label="Scenario Name" ref="scenarioName"/>
+                        <Input type="text" label="Description (Optional):" ref="description"/>
+                        <Input type="text" label="Setup Scenarios (Optional, Newline separated):" ref="setupScenarios"/>
+                        <Input type="text" label="Cleanup Scenarios (Optional, Newline separated):" ref="cleanupScenarios"/>
                     </form>
                 </div>
                 <div className="modal-footer" data-ignore="true">
-                    <Button className="pull-left" ref="resetDefaultsButton" onClick={this.cancel} data-ignore="true">Cancel</Button>
-                    <Button ref="continueButton" onClick={this.continueScenario} data-ignore="true">Continue</Button>
-                    <Button bsStyle="primary" ref="saveButton" onClick={this.saveScenario} data-ignore="true">Save</Button>
+                    <Button className="pull-left" ref="resetDefaultsButton" onClick={this.cancel}>Cancel</Button>
+                    <Button ref="continueButton" onClick={this.continueScenario}>Continue</Button>
+                    <Button bsStyle="primary" ref="saveButton" onClick={this.saveScenario}>Save</Button>
                 </div>
             </Modal>
         );
     },
 
-    cancel: function () {
+    cancel () {
         this.props.onClose();
     },
 
-    saveScenario: function (e) {
+    saveScenario (e) {
         var form = this.refs.form;
         var setup = this.refs.setupScenarios.getValue();
         var cleanup = this.refs.cleanupScenarios.getValue();
@@ -53,7 +56,7 @@ var CreateModal = React.createClass({
         });
     },
 
-    continueScenario: function () {
+    continueScenario () {
         this.props.onClose({ action: 'continue'});
     }
 });
